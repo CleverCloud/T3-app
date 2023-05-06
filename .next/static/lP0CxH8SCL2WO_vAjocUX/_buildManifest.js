253531a94ab1b36f27edb32145cc139aed91e559
julia <julia.navarro@clever-cloud.com>
2023-05-06 21:31:15 +0200

    include build repository

---
+++ .next/static/lP0CxH8SCL2WO_vAjocUX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f80f0d6a5fc5d41a.css","static/chunks/pages/index-1cdc9f4af3722855.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
